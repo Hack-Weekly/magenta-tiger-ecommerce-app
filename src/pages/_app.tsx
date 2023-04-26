517b33fd753036354fe257bea8eface30c459a41
Swappnet <advance11ua@gmail.com>
2023-04-26 21:17:30 +0300

    build fix test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import GlobalStyle from '@/styles/GlobalStyle';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Head>
-        <title>TigerTrade</title>
-      </Head>
       <Component {...pageProps} />
     </>
   );
