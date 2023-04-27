5e815c9c0ad09fdf1ded524fbe62db3aa5047d92
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-04-27 17:52:25 +0100

    Addressed comments

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -26,42 +26,40 @@ function Login() {
     }, [user, loading]);
 
     return (
-        <>
-            <StyledLoginContainer>
-                <StyledLoginBox>
-                    <StyledTextContainer>
-                        <StyledTitleRowContainer>
-                            <h1>Sign in</h1>
-                            <a href="#">I don&apos;t have an account</a>
-                        </StyledTitleRowContainer>
-                        <input placeholder="Email" type="text" />
-                        <input placeholder="Password" type="password" />
-                        <button>Continue</button>
-                    </StyledTextContainer>
-                    <StyledGoogleLoginButton
-                        onClick={() => {
-                            signInWithGoogle();
-                        }}
-                    >
-                        <FontAwesomeIcon icon={faGoogle} width={"1.1rem"} />{" "}
-                        Sign in with Google
-                        <div>
-                            <FontAwesomeIcon
-                                icon={faChevronRight}
-                                width={"0.8rem"}
-                            />
-                        </div>
-                    </StyledGoogleLoginButton>
-                    <StyledParagraph>
-                        This site is protect by the Google{" "}
-                        <a href="#">Privacy Policy</a>{" "}
-                        <a href="#">Terms and Conditions</a> -{" "}
-                        <a href="#">Privacy Policy</a> -{" "}
-                        <a href="#">CA Privacy Policy</a>
-                    </StyledParagraph>
-                </StyledLoginBox>
-            </StyledLoginContainer>
-        </>
+        <StyledLoginContainer>
+            <StyledLoginBox>
+                <StyledTextContainer>
+                    <StyledTitleRowContainer>
+                        <h1>Sign in</h1>
+                        <a href="#">I don&apos;t have an account</a>
+                    </StyledTitleRowContainer>
+                    <input placeholder="Email" type="email" />
+                    <input placeholder="Password" type="password" />
+                    <button>Continue</button>
+                </StyledTextContainer>
+                <StyledGoogleLoginButton
+                    onClick={() => {
+                        signInWithGoogle();
+                    }}
+                >
+                    <FontAwesomeIcon icon={faGoogle} width={"1.1rem"} /> Sign in
+                    with Google
+                    <div>
+                        <FontAwesomeIcon
+                            icon={faChevronRight}
+                            width={"0.8rem"}
+                        />
+                    </div>
+                </StyledGoogleLoginButton>
+                <StyledParagraph>
+                    This site is protect by the Google{" "}
+                    <a href="#">Privacy Policy</a>{" "}
+                    <a href="#">Terms and Conditions</a> -{" "}
+                    <a href="#">Privacy Policy</a> -{" "}
+                    <a href="#">CA Privacy Policy</a>
+                </StyledParagraph>
+            </StyledLoginBox>
+        </StyledLoginContainer>
     );
 }
 
